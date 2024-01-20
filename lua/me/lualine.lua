abf04f279b7cd32b73d87ec8ddee31a12785c115
Guilherme Costa <echina725@gmail.com>
2024-01-20 19:43:20 -0300

    lsp_plugin configuration added

--- lua/me/lualine.lua
+++ lua/me/lualine.lua
@@ -2,7 +2,7 @@ require('lualine').setup {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', file_status = true, path = 2 } },
+        lualine_c = { { 'filename', file_status = true, path = 1 } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress', 'selectioncount' },
         lualine_z = { 'location' }
