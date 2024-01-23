26cdb206c1ec58db1594ac04f829dbc45ed2b23d
Guilherme Costa <echina725@gmail.com>
2024-01-22 23:56:26 -0300

    float window adjustment on lsp signature

--- lua/me/lsp_signature.lua
+++ lua/me/lsp_signature.lua
@@ -8,13 +8,13 @@
   wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
-  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
+  floating_window_above_cur_line = false, -- try to place the floating above the current line when possible Note:
   -- will set to true when fully tested, set to false will use whichever side has more space
   -- this setting will be helpful if you do not want the PUM and floating win overlap
 
-  floating_window_off_x = 1, -- adjust float windows x position.
+  floating_window_off_x = 0, -- adjust float windows x position.
                              -- can be either a number or function
-  floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
+  floating_window_off_y = 2, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
                               -- can be either number or function, see examples
 
   close_timeout = 4000, -- close floating window after ms when laster parameter is entered
